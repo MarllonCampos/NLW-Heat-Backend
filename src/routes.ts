1c9801f922d402309406616aeff2cd1728b52e60
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-19 01:55:06 -0300

    feat: criado uma nova rota para a criação das mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
-import { Router } from 'express'
-import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
-const router = Router()
+const router = Router();
 
-router.post('/authenticate', new AuthenticateUserController().handle)
+router.post(
+  "/authenticate",
+  new AuthenticateUserController().handle
+);
 
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
-export {router}
+export { router };
