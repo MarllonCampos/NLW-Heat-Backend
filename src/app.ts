e5cb180259bd60b7988cf02a85dff0d7367e04de
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-10-19 02:54:22 -0300

    feat: criado um novo servidor socket, e ao conectar realizando console.log

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import "dotenv/config";
 import express from "express";
 import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 app.use(express.json());
 
@@ -25,4 +35,4 @@ app.get("/signin/callback", (req, res) => {
 
   return res.json(code);
 });
-app.listen(4000, () => console.log("Server Running"));
+export { serverHttp, io };
